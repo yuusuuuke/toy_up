49c617c7fafbcf0591e8ad9a2d975ceb4af68ada
EC2 Default User <ec2-user@ip-172-31-9-195.ap-northeast-1.compute.internal>
2023-01-05 12:29:24 +0000

    exct:micropost

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_04_084602) do
+ActiveRecord::Schema.define(version: 2023_01_05_053011) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
